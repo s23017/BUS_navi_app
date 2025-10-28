75cbc32122d394b6f21a76c01667b7892617e2f9
itc-23021 <s23021@std.it-college.ac.jp>
2025-10-28 10:22:41 +0900

    a

--- pokebus_app/src/app/page.tsx
+++ pokebus_app/src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
             aria-hidden
             src="/globe.svg"
             alt="Globe icon"
-            width={16}
+            width={17}
             height={16}
           />
           Go to nextjs.org →
