549bf136cc2b507b1d60dbd2b01e75745f6ed337
itc-23021 <s23021@std.it-college.ac.jp>
2025-12-02 19:27:19 +0900

    ディレクトリを指定した場合、ディレクトリ内すべてが修正されるように

--- pokebus_app/command/kakumei.sh
+++ pokebus_app/command/kakumei.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-node command/omas.js "$1"
+
+if [ -d "$1" ]; then
+  # ディレクトリが指定された場合、その中のすべてのファイルを処理
+  for file in "$1"/*; do
+    if [ -f "$file" ]; then
+      echo "処理中: $file"
+      node command/omas.js "$file"
+    fi
+  done
+else
+  # 単一ファイルが指定された場合
+  node command/omas.js "$1"
+fi
